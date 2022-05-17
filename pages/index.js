7b914610903ab6b1a81d324efc7f54a6220082a3
Huseyin GOZUTOK <hgozutok@gmail.com>
2022-05-17 11:48:39 +0300

    .

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>Welcome to HugoNFT Deployment Page</h1>
+        <h1 className={styles.title}>Welcome to HugoNFT Deployment Page...</h1>
       </main>
     </div>
   );
